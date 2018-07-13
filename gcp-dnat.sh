8b01d271e97b7506dc86ac53e5b8293fbfb20fbd
Kris Celmer <35005462+kris-at-occ@users.noreply.github.com>
2018-07-13 23:38:02 +0200

    Update gcp-dnat.sh

--- gcp-dnat.sh
+++ gcp-dnat.sh
@@ -5,4 +5,4 @@
 myip=$(curl -H "Metadata-Flavor: Google" http://metadata.google.internal/computeMetadata/v1/instance/network-interfaces/0/ip)
 echo 1 > /proc/sys/net/ipv4/ip_forward
 iptables -F -t nat
-iptables -t nat  -A PREROUTING -d $myip/32 -p tcp -m tcp --syn -m multiport --dports 80,443 -j DNAT --to-destination 172.29.236.100
+iptables -t nat  -A PREROUTING -d $myip/32 -p tcp -m tcp --syn -m multiport --dports 80,443,6082 -j DNAT --to-destination 172.29.236.100
